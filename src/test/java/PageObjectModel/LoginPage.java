32f83497208b8837f089f58b536a9b923395a5ca
Nabanjan Das <startyupllc@gmail.com>
2023-01-04 15:48:49 -0800

    Update to login test

---
+++ src/test/java/PageObjectModel/LoginPage.java
@@ -0,0 +1,19 @@
+package PageObjectModel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    public static void enterLogin(WebDriver driver, String phNum) {
+        driver.findElement(By.xpath(
+                        "/html/body/div/div/div/div[3]/div/div/div/div[1]/div/div/div/div[3]/div/div/div/input"))
+                .sendKeys(phNum);
+    }
+
+    public static void enterPassword(WebDriver driver, String password) {
+        driver.findElement(By.xpath(
+                        "/html/body/div/div/div/div[3]/div/div/div/div[1]/div/div/div/div[4]/div/div/div/input"))
+                .sendKeys(password);
+    }
+}
--- src/test/java/SanityTest.java
+++ src/test/java/SanityTest.java
@@ -1,3 +1,4 @@
+import PageObjectModel.LoginPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -16,15 +17,22 @@ public class SanityTest {
     private final String driverPath = "src/test/resources/chromedriver";
     private WebDriver driver;
 
-
-    private WebElement waitForElement(String elemStr) throws InterruptedException {
-        Thread.sleep(10000);
+    private WebElement waitForElementInternal(String elemStr, int sleepInmillis) throws InterruptedException {
+        Thread.sleep(sleepInmillis);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         By elemLocator = By.xpath(elemStr);
         WebElement elem = driver.findElement(elemLocator);
         // wait till the logout button is visible
         wait.until(ExpectedConditions.visibilityOf(elem));
         return elem;
+    }
+
+    private WebElement waitForElement(String elemStr) throws InterruptedException {
+        return waitForElementInternal(elemStr, 10000);
+    }
+
+    private WebElement waitForElement(String elemStr, int sleepInmillis) throws InterruptedException {
+        return waitForElementInternal(elemStr, sleepInmillis);
     }
 
     @BeforeTest
@@ -53,12 +61,8 @@ public class SanityTest {
 
     @Test(priority = 0)
     public void login() throws InterruptedException {
-        driver.findElement(By.xpath(
-                "/html/body/div/div/div/div[3]/div/div/div/div[1]/div/div/div/div[3]/div/div/div/input"))
-                .sendKeys("9958895489");
-        driver.findElement(By.xpath(
-                "/html/body/div/div/div/div[3]/div/div/div/div[1]/div/div/div/div[4]/div/div/div/input"))
-                .sendKeys("123456");
+        LoginPage.enterLogin(driver, "9958895489");
+        LoginPage.enterPassword(driver, "123456");
         driver.findElement(
                 By.xpath(
                         "/html/body/div/div/div/div[3]/div/div/div/div[1]/div/div/div/div[6]/button"))
@@ -67,7 +71,7 @@ public class SanityTest {
         elem.click();
         elem = waitForElement("//*[@id=\"root\"]/div/div[2]/div/div[2]/div/div[3]/div[3]/button");
         elem.click();
-        waitForElement("//*[@id=\"root\"]/div[3]/div/div[5]/div/div[1]/div/div[1]/div");
+        waitForElement("//*[@id=\"root\"]/div[3]/div/div[5]/div/div[1]/div/div[1]/div", 15000);
     }
 
     /*@Test (priority = 1)
