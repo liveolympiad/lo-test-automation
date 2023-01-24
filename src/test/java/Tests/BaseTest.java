fecd2f65c95d6e4247e276a5a8ae5542d0e96f62
Nabanjan Das <startyupllc@gmail.com>
2023-01-24 00:05:16 -0800

    Added headless chrome

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -1,10 +1,7 @@
 package Tests;
 
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.sun.xml.internal.bind.v2.model.core.EnumConstant;
-import org.openqa.selenium.WebDriver;
-
 import Utils.Utils;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.Browser;
 
 public class BaseTest {
