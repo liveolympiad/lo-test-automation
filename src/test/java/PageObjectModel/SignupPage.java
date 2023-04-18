32626179d1cad678bbeef043465503ce8fc472e7
Devooshka <dawnmitri@gmail.com>
2023-04-19 00:00:40 +0530

    Mob Verification

--- src/test/java/PageObjectModel/SignupPage.java
+++ src/test/java/PageObjectModel/SignupPage.java
@@ -1,2 +1,26 @@
-package PageObjectModel;public class SignupPage {
+package PageObjectModel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class SignupPage extends AbstractPage {
+    public static void enterSignup(WebDriver driver, String phoneNo) throws InterruptedException {
+        WebElement elem = driver.findElement(By.xpath(
+                        "//input[@id=':r2:']"));
+
+        elem.sendKeys(Keys.CONTROL + "a");
+        elem.sendKeys(Keys.DELETE);
+
+        //elem.clear();
+        Thread.sleep(2000);
+        elem.sendKeys(phoneNo);
+
+    }
+
+    public static void clickSignup(WebDriver driver){
+
+        driver.findElement(By.xpath("//button[@id='clicked']")).click();
+    }
 }
