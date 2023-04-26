38096b027b714fefc8ba2548397a806a6573755a
Devooshka <dawnmitri@gmail.com>
2023-04-27 00:15:59 +0530

    new tests

---
+++ src/test/java/PageObjectModel/ForgotPasswordPage.java
@@ -0,0 +1,26 @@
+package PageObjectModel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ForgotPasswordPage extends AbstractPage {
+
+    public static void clickForgotPassword(WebDriver driver) throws InterruptedException {
+        driver.findElement(By.linkText("Forgot Password?")).click();
+    }
+    public static void forgotPasswordFields(WebDriver driver, String phoneno) throws InterruptedException {
+        WebElement elem = driver.findElement(By.xpath(
+                "//input[@id=\":r0:\"]"));
+        elem.sendKeys(Keys.CONTROL + "a");
+        elem.sendKeys(Keys.DELETE);
+        elem.sendKeys(phoneno);
+
+
+    }
+    public static void clickNext(WebDriver driver){
+        driver.findElement(By.xpath("//button[@id=\"clicked\"]")).click();
+    }
+
+}
