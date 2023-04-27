bf35469fe12f163889843f22cf5211035097f6f0
Devooshka <dawnmitri@gmail.com>
2023-04-27 21:18:33 +0530

    tests

--- src/test/java/PageObjectModel/MobileVerification.java
+++ src/test/java/PageObjectModel/MobileVerification.java
@@ -7,25 +7,24 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 public class MobileVerification extends AbstractPage {
-public static void otpfields(WebDriver driver, String otp) throws InterruptedException {
-    WebElement elem = driver.findElement(By.xpath(
-            "//div[@class= 'MuiBox-root css-6tgr00'] //input[@type='tel']"));
+    public static void otpfields(WebDriver driver, String otp) throws InterruptedException {
 
-    for (int i = 0; i < 7; i++) {
-        if (elem != null) {
-
-            elem.sendKeys(Keys.BACK_SPACE);
-            //elem.sendKeys(otp);
+        for (int i = 1; i < 7; i++) {
+            WebElement elem = driver.findElement(By.xpath(
+                    "//*[@id=\"root\"]/div/div/div[3]/div/div/div/div[3]/div/div/div[" + Integer.toString(i) + "]/input"));
+            if (elem != null) {
+                elem.clear();
+            }
         }
-    }
 
-    //elem.clear();
-    elem.sendKeys(otp);
-}
+        WebElement elem = driver.findElement(By.xpath(
+                "//div[@class= 'MuiBox-root css-6tgr00'] //input[@type='tel']"));
+        elem.sendKeys(otp);
+    }
     public static void clickVerify(WebDriver driver) {
         driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[3]/div/div/div/div[4]/div/button")).click();
     }
     public static void clickClose(WebDriver driver){
-    driver.findElement(By.xpath("//button[@class='MuiButtonBase-root MuiIconButton-root MuiIconButton-colorInherit MuiIconButton-sizeSmall css-p4uye0']")).click();
+        driver.findElement(By.xpath("//button[@class='MuiButtonBase-root MuiIconButton-root MuiIconButton-colorInherit MuiIconButton-sizeSmall css-p4uye0']")).click();
     }
 }
